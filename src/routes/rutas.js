75f0a8c8b6b19b4ce4dfdec527f7a9b0463c3633
SamuelVasquezGonzalez <samuelgonza2006@gmail.com>
2022-07-19 10:13:17 -0500

    Se agrego la libreria Waypoints para realizar movimiento en el pseudoelemento ::before de los elementos li, ahora se mueven dependiendo de la seccion que este a la vista, el menu de navegacion es flotante y pegajoso a la vista

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,42 +1,44 @@
-const app = require('../config/server');
-require('colors')
-const mysqlConnection = require('../config/db');
+const app = require("../config/server");
+require("colors");
+const mysqlConnection = require("../config/db");
 
-app.get('/', (req, res)=>{
-    res.render('../views/index.ejs');
+app.get("/", (req, res) => {
+  res.render("../views/index.ejs");
 });
 
-
-app.get('/correos', (req, res) =>{
-    mysqlConnection.query('SELECT * FROM correos', (err, result) => {
-        if(err){
-            console.log('ERROR'.bgRed)
-        }else{
-            res.render('../views/correos.ejs', {
-                usuarios: result
-            });
-        }
-    });
+app.get("/correos", (req, res) => {
+  mysqlConnection.query("SELECT * FROM correos", (err, result) => {
+    if (err) {
+      console.log("ERROR".bgRed);
+    } else {
+      res.render("../views/correos.ejs", {
+        usuarios: result,
+      });
+    }
+  });
 });
 
-app.post('/correos', (req, res)=>{
-    const { nombre, correo, mensaje} = req.body;
-    mysqlConnection.query('INSERT INTO correos SET?', {
-        nombre,
-        correo,
-        mensaje
-    }, (err, result) =>{
-        if(err){
-            console.log(`${err}`.bgRed);
-        }
-        res.redirect('/');
-        
-    });
-})
+app.post("/correos", (req, res) => {
+  const { nombre, correo, mensaje } = req.body;
+  mysqlConnection.query(
+    "INSERT INTO correos SET?",
+    {
+      nombre,
+      correo,
+      mensaje,
+    },
+    (err, result) => {
+      if (err) {
+        console.log(`${err}`.bgRed);
+      }
+      res.redirect("/");
+    }
+  );
+});
 
-app.all('*', (req, res)=>{
-    const badUrl = req.originalUrl;
-    res.render('../views/404.ejs', {
-        ruta: badUrl
-    });
-})
+app.all("*", (req, res) => {
+  const badUrl = req.originalUrl;
+  res.render("../views/404.ejs", {
+    ruta: badUrl,
+  });
+});
--- src/static/scripts/animaciones.js
+++ src/static/scripts/animaciones.js
@@ -1,36 +1,99 @@
-let botonGirar = document.querySelector('.btn');
-let imgGirar = document.getElementById('girando')
-botonGirar.addEventListener('click', rotar);
-
+let botonGirar = document.querySelector(".btn");
+let imgGirar = document.getElementById("girando");
+botonGirar.addEventListener("click", rotar);
 
 numero = 0;
-function rotar (){
-    imgGirar.style.transform = `rotate(${numero += -60}deg)`;
+function rotar() {
+  imgGirar.style.transform = `rotate(${(numero += -60)}deg)`;
 }
 
 // ------- cambiar imagen -----
 
-let imagenGirar = document.querySelector('.animacion');
-let yo = document.querySelector('.yo');
-let esqueleto = document.querySelector('.esqueleto');
+let imagenGirar = document.querySelector(".animacion");
+let yo = document.querySelector(".yo");
+let esqueleto = document.querySelector(".esqueleto");
 
-imagenGirar.addEventListener('click', cambiar);
+imagenGirar.addEventListener("click", cambiar);
 
-function cambiar () {
-    yo.classList.toggle('ocultado');
-    esqueleto.classList.toggle('ocultado')
+function cambiar() {
+  yo.classList.toggle("ocultado");
+  esqueleto.classList.toggle("ocultado");
 }
 
-let aButton = document.querySelector('.si')
-if(screen.width < 399){
-    aButton.innerHTML = 'Yo';
-
-}else{
-    aButton.innerHTML = 'Acerca de';
+let aButton = document.querySelector(".si");
+if (screen.width < 399) {
+  aButton.innerHTML = "Yo";
+} else {
+  aButton.innerHTML = "Acerca de";
 }
 
-let changeText = document.querySelector('#changeText')
-btnEnviar.addEventListener('click', ()=>{
-    changeText.innerText = 'Correo Enviado Correctamente :)'
+let cero = document.getElementById("cero");
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
 
-})
+let liList = document.querySelectorAll(".li");
+
+var waypointCero = new Waypoint({
+  element: cero,
+  handler: function (direction) {
+    liList[0].classList.toggle("activo");
+    if (direction == "up") {
+      liList[1].classList.remove("activo");
+    }
+  },
+});
+
+var waypointOne = new Waypoint({
+  element: one,
+  handler: function (direction) {
+    liList[1].classList.toggle("activo");
+    if (direction == "down") {
+      liList[0].classList.remove("activo");
+    }
+    if (direction == "up") {
+      liList[2].classList.remove("activo");
+    }
+  },
+  offset: 95,
+});
+
+var waypointTwo = new Waypoint({
+  element: two,
+  handler: function (direction) {
+    liList[2].classList.toggle("activo");
+    if (direction == "down") {
+      liList[1].classList.remove("activo");
+    }
+    if (direction == "up") {
+      liList[3].classList.remove("activo");
+    }
+  },
+  offset: 95,
+});
+
+var waypointThree = new Waypoint({
+  element: three,
+  handler: function (direction) {
+    liList[3].classList.toggle("activo");
+    if (direction == "down") {
+      liList[2].classList.remove("activo");
+    }
+    if (direction == "up") {
+      liList[4].classList.remove("activo");
+    }
+  },
+  offset: 95,
+});
+
+var waypointFour = new Waypoint({
+  element: four,
+  handler: function (direction) {
+    liList[4].classList.toggle("activo");
+    if (direction == "down") {
+      liList[3].classList.remove("activo");
+    }
+  },
+  offset: 97,
+});
