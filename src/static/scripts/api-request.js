7a23806a6271327d748379bd49d46fb0951b7efb
SamuelVasquezGonzalez <samuelgonza2006@gmail.com>
2023-03-12 11:18:52 -0500

    Manejo de errores en caso de que no carguen las imagenes

--- src/static/scripts/api-request.js
+++ src/static/scripts/api-request.js
@@ -1,6 +1,10 @@
 fetch('https://samuelvasquezgonzalez.github.io/API-portf/api-habilidades.json')
   .then((response) => response.json())
   .then((data) => guardarData(data))
+  .catch((error) => errorLo(error))
+
+  const routeError = '../multimedia/error.png';
+  const errorMsm = 'No hemos podido cargar la información :(';
 
   let desarrolloWeb = document.querySelector(".webTools");
   let diseñoWeb = document.querySelector(".desingWeb");
@@ -33,27 +37,73 @@ const guardarData = (data) => {
         contenedor.appendChild(tarjeta)
       
   }
+    crearTarjeta(desarrolloWeb, data[0].html.imagen, data[0].html.parrafo)
+    crearTarjeta(desarrolloWeb, data[0].css.imagen, data[0].css.parrafo)
+    crearTarjeta(desarrolloWeb, data[0].js.imagen, data[0].js.parrafo)
 
-  crearTarjeta(desarrolloWeb, data[0].html.imagen, data[0].html.parrafo)
-  crearTarjeta(desarrolloWeb, data[0].css.imagen, data[0].css.parrafo)
-  crearTarjeta(desarrolloWeb, data[0].js.imagen, data[0].js.parrafo)
+    crearTarjeta(diseñoWeb, data[1].fontAwesomeIcons.imagen, data[1].fontAwesomeIcons.parrafo)
+    crearTarjeta(diseñoWeb, data[1].googleIcons.imagen, data[1].googleIcons.parrafo)
+    crearTarjeta(diseñoWeb, data[1].ps.imagen, data[1].ps.parrafo)
+    crearTarjeta(diseñoWeb, data[1].ai.imagen, data[1].ai.parrafo)
+    
+    crearTarjeta(baseDatos, data[2].mysql.imagen, data[2].mysql.parrafo)
 
-  crearTarjeta(diseñoWeb, data[1].fontAwesomeIcons.imagen, data[1].fontAwesomeIcons.parrafo)
-  crearTarjeta(diseñoWeb, data[1].googleIcons.imagen, data[1].googleIcons.parrafo)
-  crearTarjeta(diseñoWeb, data[1].ps.imagen, data[1].ps.parrafo)
-  crearTarjeta(diseñoWeb, data[1].ai.imagen, data[1].ai.parrafo)
-  
-  crearTarjeta(baseDatos, data[2].mysql.imagen, data[2].mysql.parrafo)
+    crearTarjeta(herramientas, data[3].git.imagen, data[3].git.parrafo)
+    crearTarjeta(herramientas, data[3].npm.imagen, data[3].npm.parrafo)
+    crearTarjeta(herramientas, data[3].terminal.imagen, data[3].terminal.parrafo)
+    crearTarjeta(herramientas, data[3].vsc.imagen, data[3].vsc.parrafo)
+    crearTarjeta(herramientas, data[3].workbench.imagen, data[3].workbench.parrafo)
+    crearTarjeta(herramientas, data[3].xampp.imagen, data[3].xampp.parrafo)
 
-  crearTarjeta(herramientas, data[3].git.imagen, data[3].git.parrafo)
-  crearTarjeta(herramientas, data[3].npm.imagen, data[3].npm.parrafo)
-  crearTarjeta(herramientas, data[3].terminal.imagen, data[3].terminal.parrafo)
-  crearTarjeta(herramientas, data[3].vsc.imagen, data[3].vsc.parrafo)
-  crearTarjeta(herramientas, data[3].workbench.imagen, data[3].workbench.parrafo)
-  crearTarjeta(herramientas, data[3].xampp.imagen, data[3].xampp.parrafo)
-
-  crearTarjeta(backend, data[4].nodeJs.imagen, data[4].nodeJs.parrafo)
-  crearTarjeta(backend, data[4].express.imagen, data[4].express.parrafo)
-
-
+    crearTarjeta(backend, data[4].nodeJs.imagen, data[4].nodeJs.parrafo)
+    crearTarjeta(backend, data[4].express.imagen, data[4].express.parrafo)
 }
+
+
+const errorLo = (error) =>{
+  function crearTarjeta (contenedor, imagen, parrafo){
+    // Crear tarejeta
+      let tarjeta = document.createElement("div");
+      tarjeta.classList.add("habilidad");
+      tarjeta.classList.add("headline");
+
+      let img = document.createElement("img");
+      img.classList.add("img-comunes");
+      img.classList.add("pequeña");
+      img.src = imagen;
+
+      let p = document.createElement("p");
+      p.classList.add("parrado");
+      p.classList.add("negrilla");
+      p.classList.add("hab");
+      p.innerHTML = parrafo;
+
+      tarjeta.appendChild(img);
+      tarjeta.appendChild(p);
+
+      contenedor.appendChild(tarjeta)
+    
+}
+  crearTarjeta(desarrolloWeb, routeLoadError, errorMsm)
+  crearTarjeta(desarrolloWeb, routeLoadError, errorMsm)
+  crearTarjeta(desarrolloWeb, routeLoadError, errorMsm)
+
+  crearTarjeta(diseñoWeb, routeLoadError, errorMsm)
+  crearTarjeta(diseñoWeb, routeLoadError, errorMsm)
+  crearTarjeta(diseñoWeb, routeLoadError, errorMsm)
+  crearTarjeta(diseñoWeb, routeLoadError, errorMsm)
+  
+  crearTarjeta(baseDatos, routeLoadError, errorMsm)
+
+  crearTarjeta(herramientas, routeLoadError, errorMsm)
+  crearTarjeta(herramientas, routeLoadError, errorMsm)
+  crearTarjeta(herramientas, routeLoadError, errorMsm)
+  crearTarjeta(herramientas, routeLoadError, errorMsm)
+  crearTarjeta(herramientas, routeLoadError, errorMsm)
+  crearTarjeta(herramientas, routeLoadError, errorMsm)
+
+  crearTarjeta(backend, routeLoadError, errorMsm)
+  crearTarjeta(backend, routeLoadError, errorMsm)
+  console.log(error)
+}
+
--- src/static/scripts/media-request.js
+++ src/static/scripts/media-request.js
@@ -1,10 +1,12 @@
 fetch('https://samuelvasquezgonzalez.github.io/API-portf/media.json')
   .then((response) => response.json())
   .then((data) => guardarMedia(data))
+  .catch((error) => loadGifError(error));
+
+const routeLoadError = '../multimedia/error.png';
 
 let skeleton = document.querySelector(".banskeleton")
 let yo = document.querySelector(".banyo")
-let luna = document.querySelector(".banluna")
 let proyectos = document.querySelectorAll(".pr")
 let skater = document.querySelector(".skater")
 let skeletonGif = document.querySelector(".skeletonS")
@@ -14,7 +16,6 @@ const guardarMedia = (data) => {
 
     skeleton.src = data[0].bannerSkeleton;
     yo.src = data[0].bannerImg;
-    luna.src = data[0].moon;
 
     proyectos[0].src = data[0].lmGalery;
     proyectos[1].src = data[0].dados;
@@ -26,5 +27,21 @@ const guardarMedia = (data) => {
     skeletonGif.src = data[0].skeleton;
     vector.src = data[0].vector;
 
+}
 
+const loadGifError = (error) =>{
+  skeleton.src = routeLoadError;
+  yo.src = routeLoadError;
+
+  proyectos[0].src = routeLoadError;
+  proyectos[1].src = routeLoadError;
+  proyectos[2].src = routeLoadError;
+  proyectos[3].src = routeLoadError;
+
+  proyectos.src = routeLoadError;
+  skater.src = routeLoadError;
+  skeletonGif.src = routeLoadError;
+  vector.src = routeLoadError;
+
+  console.log(error)
 }
